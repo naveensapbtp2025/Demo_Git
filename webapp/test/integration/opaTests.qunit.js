18358878909562c08f6f20c2dd7311608aa3c4db
Naveen S <naveenssn2013@gmail.com>
2025-08-06 12:22:23 +0000

    Initial version

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["demoproject/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"demoproject/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
